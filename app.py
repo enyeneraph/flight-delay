d95c673c6d35516b624999e3f0e09661c5d05d23
enyeneraph <enyenerudolf@gmail.com>
2022-10-07 23:31:25 +0100

    made some updates

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from predictions import predict
 file = open('categories.json')
 data = json.load(file)
 
+
 def decode_category(ele, data=data):
     return data[ele].values()
 
@@ -21,30 +22,24 @@ previous_airports = decode_category("PREVIOUS_AIRPORT")
 
 st.title('Predicting Flight Delays')
 st.markdown('Model for predicting if a flight will be delayed or not.')
-# "MONTH_NAME", "DAY_NAME", "CARRIER_NAME", "DEPARTING_AIRPORT", "PREVIOUS_AIRPORT"
 
 st.header('Flight Features')
 col1, col2, col3 = st.columns(3) #need to reset columns to accomodate five features
 with col1:
-    # st.text('Sepal characteristics')
     month = st.selectbox('Select month', months)
-    # day = st.selectbox('Select day', days)
-    # previous_airport = st.selectbox("Select previous_airport", previous_airports)
+
 with col2:
     previous_airport = st.selectbox("Select previous_airport", previous_airports)
     day = st.selectbox('Select day', days)
 with col3:
-    # st.text('Pepal characteristics')
     carrier_name = st.selectbox('Select Airport Carrier', carriers)
     departing_airport = st.selectbox('Select departing airport', departing_airports) #need categories + code for category encoding
-    # previous_airport = st.selectbox("Select previous_airport", previous_airports)
 
 def uncover(column, value):
     for ele in data[column].items():
         if value == ele[1]:
-            # print(ele[1], int(ele[0]))
             return int(ele[0])
-            print(value, ele[0])
+            # print(value, ele[0])
 
 month = uncover("MONTH_NAME", month)
 day = uncover("DAY_NAME", day)
